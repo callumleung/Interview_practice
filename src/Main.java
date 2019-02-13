b9ac5e89e942b5787a1cc517fa62da7bdf40d99d
Callum <callum.leung@gmail.com>
2019-02-13 10:59:52 +0000

    int

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Main {
 
@@ -37,7 +34,7 @@ public class Main {
        /*char[] testChar = {'a','b','c','a','b'};
        System.out.println(removeDuplicates(testChar));*/
 
-       //String[] testString = {"hello", "there", "general", "kenobi", "hello", "there"};
+        //String[] testString = {"hello", "there", "general", "kenobi", "hello", "there"};
        /* int[]  testInt = {1,2,3,4,5,6};
         int[] reversed = reverseArray(testInt);
         for (int i: reversed){
@@ -51,7 +48,7 @@ public class Main {
       String test2 = "abcdef";
       System.out.println(isAnagram(test1, test2));*/
 
-      //System.out.println(reverseInt(12345));
+        //System.out.println(reverseInt(12345));
 
         //System.out.println(firstNonRepeatedLetter("Hello there, General Kenobi"));
 
@@ -62,22 +59,28 @@ public class Main {
         testLinkedList.add(4);
         System.out.println(middleOfLinkList(testLinkedList));*/
 
+      /*System.out.println(sqrt(4));
+      System.out.println(sqrt(16));
+      System.out.println(sqrt(9));
+      System.out.println(sqrt(3));*/
 
+      //System.out.println(removeChar("Hello there, General Kobi", 'l'));
 
+        //printFib100();
 
-
+        fizzBuzz(50);
     }
 
-    private static void printFib(int limit){
+    private static void printFib(int limit) {
 
         int a = 1;
         int b = 1;
-        System.out.println(a +"\n" +b);
+        System.out.println(a + "\n" + b);
 
-        while( b+a < limit){
+        while (b + a < limit) {
 
             int initialB = b;
-            b = a+b;
+            b = a + b;
             a = initialB;
             System.out.println(b);
 
@@ -85,14 +88,14 @@ public class Main {
 
     }
 
-    private static boolean checkPrime(int num){
+    private static boolean checkPrime(int num) {
 
         //check all numbers from 2 up to num/2
         //if one then number is not prime
 
-        for (int i = 2; i < num/2; i++){
+        for (int i = 2; i < num / 2; i++) {
             //divides
-            if (num%i == 0){
+            if (num % i == 0) {
                 return false;
             }
         }
@@ -101,39 +104,39 @@ public class Main {
 
     }
 
-    private static boolean checkPalindrome(String input){
+    private static boolean checkPalindrome(String input) {
 
-        for (int i = 0; i < input.length(); i++){
+        for (int i = 0; i < input.length(); i++) {
 
             //complementary char at the other end of string
-            int j =  input.length() - i -1;
+            int j = input.length() - i - 1;
 
             //not matching, therefore not palindromic
-            if (input.charAt(i) != input.charAt(j)){
+            if (input.charAt(i) != input.charAt(j)) {
                 return false;
             }
 
         }
 
         //if not exited yet, must be true
-        return  true;
+        return true;
     }
 
-    private static boolean checkIntPalindrome(int value){
+    private static boolean checkIntPalindrome(int value) {
 
         //store a copy
         int copy = value;
         int reverse = 0;
 
-        while (copy != 0){
+        while (copy != 0) {
 
             int remainder = copy % 10;
-            reverse = reverse*10 + remainder;
-            copy = copy/10; //int so remainder is tossed
+            reverse = reverse * 10 + remainder;
+            copy = copy / 10; //int so remainder is tossed
 
         }
 
-        if (value == reverse){
+        if (value == reverse) {
             return true;
         } else {
             return false;
@@ -141,37 +144,37 @@ public class Main {
 
     }
 
-    private static boolean checkArmstrong(int value){
+    private static boolean checkArmstrong(int value) {
 
         int input = value;
         ArrayList<Integer> digits = new ArrayList<>();
 
-        while (input != 0){
+        while (input != 0) {
 
             int remainder = input % 10;
             digits.add(remainder);
-            input = input/10;
+            input = input / 10;
         }
 
         int sum = 0;
-        for (int i: digits){
-            sum += i*i*i;
+        for (int i : digits) {
+            sum += i * i * i;
         }
 
-        if (sum == value){
+        if (sum == value) {
             return true;
         } else {
             return false;
         }
     }
 
-    private static int calcFactorial(int value){
+    private static int calcFactorial(int value) {
 
         int sum = 1;
 
-        while (value != 0){
+        while (value != 0) {
 
-            sum = sum*value;
+            sum = sum * value;
             value--;
 
         }
@@ -179,14 +182,14 @@ public class Main {
 
     }
 
-    private static String reverseString(String input){
+    private static String reverseString(String input) {
 
         int j = input.length();
         String reversed = "";
 
-        for (int i =0; i < j ; i ++){
+        for (int i = 0; i < j; i++) {
 
-            reversed+= input.charAt(j - i -1);
+            reversed += input.charAt(j - i - 1);
 
         }
 
@@ -194,28 +197,28 @@ public class Main {
 
     }
 
-    private static char[] removeDuplicates(char[] input){
+    private static char[] removeDuplicates(char[] input) {
 
         String returnString = "";
 
-        for (int i = 0; i < input.length; i ++){
+        for (int i = 0; i < input.length; i++) {
             String currentChar = Character.toString(input[i]);
 
-            if (!returnString.contains(currentChar)){
-                returnString+=currentChar;
+            if (!returnString.contains(currentChar)) {
+                returnString += currentChar;
             }
         }
 
         return returnString.toCharArray();
     }
 
-    private static String[] removeDuplicates(String[] input){
+    private static String[] removeDuplicates(String[] input) {
 
         ArrayList<String> tempArray = new ArrayList<>();
 
-        for (int i = 0; i < input.length; i ++){
+        for (int i = 0; i < input.length; i++) {
 
-            if (!tempArray.contains(input[i])){
+            if (!tempArray.contains(input[i])) {
                 tempArray.add(input[i]);
             }
         }
@@ -224,12 +227,12 @@ public class Main {
         return returnArray;
     }
 
-    private static int[] reverseArray(int[] input){
+    private static int[] reverseArray(int[] input) {
 
-        for (int i = 0; i < input.length/2 ; i ++){
+        for (int i = 0; i < input.length / 2; i++) {
 
             int a = input[i];
-            int b = input[input.length - i-1];
+            int b = input[input.length - i - 1];
 
             input[i] = b;
             input[input.length - i - 1] = a;
@@ -238,102 +241,102 @@ public class Main {
         return input;
     }
 
-    private static String reverseSentence(String input){
+    private static String reverseSentence(String input) {
 
         String[] words = input.split(" ");
         String out = "";
 
-        for (int i = 0; i < words.length; i ++){
+        for (int i = 0; i < words.length; i++) {
 
-          out += words[words.length - i -1];
-          out += " ";
+            out += words[words.length - i - 1];
+            out += " ";
         }
 
         return out;
     }
 
-    private static boolean isAnagram(String a, String b){
+    private static boolean isAnagram(String a, String b) {
 
-        if (a.length() != b.length()){
+        if (a.length() != b.length()) {
             return false;
         }
 
-       for (int i = 0; i < a.length(); i++){
-           if (!b.contains(String.valueOf(a.charAt(i)))){
-               return false;
-           }
-           if (!a.contains(String.valueOf(b.charAt(i)))){
-               return false;
-           }
-       }
+        for (int i = 0; i < a.length(); i++) {
+            if (!b.contains(String.valueOf(a.charAt(i)))) {
+                return false;
+            }
+            if (!a.contains(String.valueOf(b.charAt(i)))) {
+                return false;
+            }
+        }
 
-       return true;
+        return true;
     }
 
-    private static int reverseInt(int input){
+    private static int reverseInt(int input) {
 
         ArrayList<Integer> digits = new ArrayList<>();
         int temp = input;
 
-        while(temp != 0){
+        while (temp != 0) {
 
             int tempDigit = temp % 10;
             digits.add(tempDigit);
-            temp = temp /10;
+            temp = temp / 10;
         }
 
         //Collections.reverse(digits);
 
-        int total =  0;
-        for (int i: digits){
-            total = total*10 + i;
+        int total = 0;
+        for (int i : digits) {
+            total = total * 10 + i;
         }
 
         return total;
     }
 
-    private static char firstNonRepeatedLetter(String input){
+    private static char firstNonRepeatedLetter(String input) {
 
         input = input.toLowerCase();
-        for (int i = 0; i < input.length(); i ++){
+        for (int i = 0; i < input.length(); i++) {
 
             boolean notRepeated = true;
 
-            for (int j = i + 1; j < input.length(); j++){
+            for (int j = i + 1; j < input.length(); j++) {
 
-                if (input.charAt(j) == input.charAt(i)){
+                if (input.charAt(j) == input.charAt(i)) {
                     notRepeated = false;
                 }
             }
-            if (notRepeated == true){
+            if (notRepeated == true) {
                 return input.charAt(i);
             }
         }
         return 'z';
     }
 
-    private static int middleOfLinkList(LinkedList<Integer> input){
+    private static int middleOfLinkList(LinkedList<Integer> input) {
 
         int midPoint = 0;
-        if (input.size() % 2 == 0){
-            midPoint = input.size()/2 -1;
-        } else if (input.size() % 2 != 0){
-            midPoint = input.size()/2 ;
+        if (input.size() % 2 == 0) {
+            midPoint = input.size() / 2 - 1;
+        } else if (input.size() % 2 != 0) {
+            midPoint = input.size() / 2;
         }
 
         return input.get(midPoint);
     }
 
-    private static int[][] matrixSum(int[][] a, int[][] b){
+    private static int[][] matrixSum(int[][] a, int[][] b) {
 
         int[][] sum = new int[a.length][a[0].length];
 
-        if (a.length!= b.length || a[0].length != b[0].length){
+        if (a.length != b.length || a[0].length != b[0].length) {
 
-            throw new  ArithmeticException("matrices not same size");
+            throw new ArithmeticException("matrices not same size");
         } else {
-            for (int i = 0; i < a.length; i ++){
-                for (int j = 0; j < a[0].length; j++){
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < a[0].length; j++) {
                     sum[i][j] = a[i][j] + b[i][j];
                 }
             }
@@ -341,10 +344,112 @@ public class Main {
         return sum;
     }
 
-    private static String removeWhiteSpace(String input){
+    private static String removeWhiteSpace(String input) {
 
-        return  input.replace("\\s+", "");
+        return input.replace("\\s+", "");
     }
+
+    private static int sqrt(int x) {
+
+        for (int i = 1; i < x / 2; i++) {
+            if (i * i == x) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private static String removeChar(String input, char c){
+        char[] chars = input.toCharArray();
+        String out = "";
+        for(char k: chars){
+            if (k != c){
+                out+=String.valueOf(k);
+            }
+        }
+        return out;
+    }
+
+    private static void printFib100(){
+
+        //fib sequence defined as f_n = f_(n-1) + f_(n-2)
+        int a = 0;
+        int b = 1;
+        int temp =0;
+        System.out.print(b);
+
+        while (b <= 89){
+            b = b + a;
+            a = b - a;
+            if (b <= 100) {
+                System.out.print(" " + b);
+            }
+
+        }
+
+
+    }
+
+    private static void fizzBuzz(int limit){
+
+        for (int i = 1; i < limit; i ++){
+
+            if (i % 3 ==0){
+                System.out.print("fizz");
+            }
+            if (i % 5 == 0){
+                System.out.print("buzz");
+            }
+            if (i % 3 != 0 && i % 5 != 0){
+                System.out.print(i);
+            }
+            System.out.print("\n");
+        }
+
+    }
+
+    private static int[] removeDuplicates(int[] input){
+
+        Set<Integer> hashedInput = new HashSet<>();
+
+        //HashSet does not admit duplicates
+        for (int i: input){
+            hashedInput.add(input[i]);
+        }
+
+        //cast back to array
+        Integer[] returnArray = hashedInput.toArray(new Integer[hashedInput.size()]);
+        int[] rA2 = new int[hashedInput.size()];
+        for (Integer i: hashedInput){
+            rA2[i] = hashedInput.
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
